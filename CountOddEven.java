43cd3d9ab68379a8285ba450a69660734834fd66
Pankaj-kurmi <pankajkurmi1408@gmail.com>
2026-04-11 10:20:30 +0530

    error fixed

--- CountOddEven.java
+++ CountOddEven.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+class CountOddEve {
     public static void main(String[] args) {
         List<Integer> li = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         CountOddEven(li);
