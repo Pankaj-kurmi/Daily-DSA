c1b905425bfdad274e577088bb593647f6744ffc
Pankaj-kurmi <pankajkurmi1408@gmail.com>
2026-04-11 11:20:45 +0530

    check prime or not

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+      int n = 7;
+      System.out.println(checkPrime(n));
+      System.out.println("--------------");
+      System.out.println(primee(479));
+    }
+    public static boolean checkPrime(int n){
+        if (n<=1) {
+            return false;
+        }
+        for(int i = 2 ; i<= n/2 ; i++){
+            if (n%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean primee(int n){
+        if (n<=2) {
+            return false;
+        }
+        for(int i =2 ; i*i <=n ; i++){
+            if (n%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
