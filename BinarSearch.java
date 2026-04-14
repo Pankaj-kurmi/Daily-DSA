22aed95ccd63f3846e94e5f7b879a9ba48cd328a
Pankaj-kurmi <pankajkurmi1408@gmail.com>
2026-04-14 22:46:40 +0530

    binay search

---
+++ BinarSearch.java
@@ -0,0 +1,25 @@
+class BinaySearch{
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10,11}; //ans 
+        int target = 1;
+        System.out.println(findtarget(arr, target));
+    }
+    public static int findtarget(int arr[] , int target){
+        int left =0;
+        int right = arr.length -1;
+        while (left<=right) {
+            int mid = (left + right) /2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if(arr[mid] < target){
+                left  = mid +1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
+    
+}
